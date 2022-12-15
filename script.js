7203dc9ff9dd97de662913106aa16c95cbefef93
Bernachea <BERNM156@corp.espn.pvt>
2022-12-15 03:08:10 -0300

    html + css finished, js in progress

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButton = document.querySelectorAll('.number');
+const operationButton = document.querySelectorAll('.operation');
+const equalButton = document.querySelector('.equal');
+const deleteButton = document.querySelector('.delete');
+const allClearButton = document.querySelector('.all-clear');
+const previousOperandText = document.querySelector('.previous-js');
+const currentOperandText = document.querySelector('.current-js');
