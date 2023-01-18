0463ad81ced1e4ed79c21e056117ec00941a8423
Bernachea <BERNM156@corp.espn.pvt>
2023-01-18 02:56:29 -0300

    calculator js finished - WORKING CALCULATOR!

--- script.js
+++ script.js
@@ -1,7 +1,122 @@
-const numberButton = document.querySelectorAll('.number');
-const operationButton = document.querySelectorAll('.operation');
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
 const equalButton = document.querySelector('.equal');
 const deleteButton = document.querySelector('.delete');
 const allClearButton = document.querySelector('.all-clear');
 const previousOperandText = document.querySelector('.previous-js');
 const currentOperandText = document.querySelector('.current-js');
+
+class Calculator {
+    constructor(previousOperandText, currentOperandText) {
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.slice(0, -1);
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return; // CHANGE ',' for '.' to have calculator in USA decimal system
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]); // CHANGE ',' for '.' to have calculator in USA decimal system
+        const decimalDigits = stringNumber.split('.')[1]; // CHANGE ',' for '.' to have calculator in USA decimal system
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0}); // CHANGE CALCULATOR LANGUAGE = toLocaleString('en')
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`; // CHANGE `${integerDisplay},${decimalDigits}` for `${integerDisplay}.${decimalDigits}` to have calculator in USA decimal system
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+        } else {
+            this.previousOperandText.innerText = '';
+        }
+    }
+}
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+equalButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
